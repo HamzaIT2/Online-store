29e420fd719b46eb2012c3e4741193cc2d22af53
HamzaIT2 <hmzhkymr4@gmail.com>
2025-10-27 01:19:58 +0300

    update BaseURL

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: process.env.REACT_APP_API_URL
+ // baseURL: "http://localhost:3001/api/v1",
 });
 
 axiosInstance.interceptors.request.use((config) => {
