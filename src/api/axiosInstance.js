14bcb3e62dc9fb1fb3f21630e60dd82ed165928e
HamzaIT2 <hmzhkymr4@gmail.com>
2025-10-27 15:33:52 +0300

    update BaseURL

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  //baseURL: import.meta.env.VITE_API_URL,
  // baseURL: process.env.REACT_APP_API_URL
- // baseURL: "http://localhost:3001/api/v1",
+  baseURL: "https://al-dawaar-backend.onrender.com/api/v1",
 });
 
 axiosInstance.interceptors.request.use((config) => {
