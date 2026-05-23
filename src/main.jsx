3dfc9cdd92da60b93e38ae84499b884e7082d80d
HamzaIT2 <hmzhkymr4@gmail.com>
2026-05-23 14:47:25 +0300

    update code

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,19 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { ThemeProvider } from "./context/ThemeContext";
+import ErrorBoundary from './components/ErrorBoundary';
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ThemeProvider>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <App />
-      </ClerkProvider>
-    </ThemeProvider>
-  </StrictMode>,
+  <ErrorBoundary>
+    <StrictMode>
+      <ThemeProvider>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+        </ClerkProvider>
+      </ThemeProvider>
+    </StrictMode>
+  </ErrorBoundary>,
 )
